80018ad6c2bd752721b67501c175a48767c7db1e
Akanksha Sharma <akanksha.sharma@crib.in>
2023-05-05 15:20:24 +0530

    Added view and tenplate for detailed blog

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from datetime import timezone
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+import pkg_resources
 
 # Create your views here.
 from .models import Post
 
 
 def post_list(request):
+    post= get_object_or_404(Post, pk =pkg_resources )
     posts = Post.objects.filter(published_date__lte= timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
-
-
 
